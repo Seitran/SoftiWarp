26bcda03074d83a17c57b79afd1d8ec72e7acc70
Simon Zhang <simonzhangsm@users.noreply.github.com>
2014-11-08 13:30:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 apt-cache search rdma
-apt-get install libtool autoconf automake linux-tool-common linux-headers-$(uname -r) ibverbs-utils libibverbs-dev libibverbs1 librdmacm-dev librdmacm1 rdmacm-utils
+apt-get install gcc make cmake libtool autoconf automake linux-tool-common linux-headers-$(uname -r) ibverbs-utils libibverbs-dev libibverbs1 librdmacm-dev librdmacm1 rdmacm-utils
